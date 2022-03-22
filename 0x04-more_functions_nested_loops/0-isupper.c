7a970fe3df1c08a3cb59629dedd3ca8eb87f4736
jacques172 <asinyojacques17@gmail.com>
2022-03-22 16:05:35 -0700

    more functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_isupper:- checks for upppercase character
+ *@c: parameter
+ *Return: return 1 if c is uppercase and 0 otherwise
+ *
+ * */
+
+int _isupper(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (0);
+
+}
+else
+{
+return (1);
+}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is the readme file for the more_functions_nested_loops
