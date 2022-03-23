f35f6b6bd19c175ea3e11c00c93533b63a88a6df
jacques172 <96381612+jacques172@users.noreply.github.com>
2022-03-23 07:53:34 +0000

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,18 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
-*print_line:- print line
-*@n: parameter consisting of the number of line
-*Return: return nothing
-*/
+ * print_line - Draws a straight line using the character _.
+ * @n: The number of _ characters to be printed.
+ */
 void print_line(int n)
 {
-  if (n <= 0)
-  {
-    _putchar('\n');
-  }
-  else
-  
-  {
-    int i;
-    for(i = 1; i < n; i++ )
-    {
-      _putchar('_');
-    }
-     _putchar('\n');
-  }
+	int len;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+			_putchar('_');
+	}
+
+	_putchar('\n');
 }
