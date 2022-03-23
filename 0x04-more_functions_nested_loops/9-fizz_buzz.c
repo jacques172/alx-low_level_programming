a3882daf3b831f10b437df979f8e2591d9935f77
jacques172 <96381612+jacques172@users.noreply.github.com>
2022-03-23 22:05:19 +0000

    Create 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+*numbers_and_words:- prints numbers and words
+*Return: returns nothing
+*/
+void numbers_and_words(void)
+{
+  int i;
+  for (i = 1; i <= 100; i++)
+  {  
+    if (i % 3 == 0)
+    {
+      printf("Fizz");
+      
+    }
+    else if (i % 5 == 0)
+    {
+      printf("Buzz");
+      \
+    }
+    else if (i % 3 == 0 && i % 5 == 0)
+    {
+      printf("FizzBuzz");
+      
+    }
+    else
+    {
+      printf("%d", i);
+      
+    }
+   printf(" ");
+  }
+}
