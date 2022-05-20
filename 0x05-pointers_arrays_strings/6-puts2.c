b15119b8efb1b0bf4e3ab732a1d264b194e03e52
Jacques Asinyo <asinyojacques17@gmail.com>
2022-05-20 09:14:24 +0000

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a  string to stdout
+ * @str: string to print
+ *
+ * Return: void
+ */
+void puts2()
+{
+int i ;
+putchar(s[0]);
+while(s[i++])
+{
+
+
+    i = i +1;
+    putchar(s[i]);
+}i
