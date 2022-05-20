f08c3702511865d7cc633add7378a56b8edaea40
Jacques Asinyo <asinyojacques17@gmail.com>
2022-05-20 08:27:29 +0000

    all

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,3 +3,35 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
+void rev_string(char *s);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
